b9972722793d31745a4da790e68efbf3cde4f7de
junhakang <junhakang5730@gmail.com>
2021-01-15 23:20:34 +0900

    친구 프로필 클릭 추가

--- weather-app/src/Molecules/FriendProfile.js
+++ weather-app/src/Molecules/FriendProfile.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import './FriendProfile.css';
-
 import profilePicture from '../Atoms/profilePicture';
 
 export function FriendProfile() {
+    
+    const [profileState, toggleState] = useState(false)
+
+    useEffect(() => {
+        document.title = `meh`;
+      });
+
     return (
     
-        <div className="background">
+        <div className={profileState ? 'background selected' : 'background'} onClick={()=>toggleState(!profileState)}>
             <profilePicture imageUrl='https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=20200603001356048249ecba8d8b8617764132.jpg' imageName="Me"/>
             <profilePicture/>
             <div className="name">
                 name
             </div>
+            {console.log(profileState)}
         </div>
         
     )
